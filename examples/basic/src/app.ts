4e840a6403d85ae48374f1728e932a35eb3e935d
Danilo Castro <dan2dev@outlook.com>
2025-12-19 23:31:18 -0300

    feat: Add feature to toggle element visibility based on checkbox input.
    - Add feature to toggle visibility of an element based on a checkbox input.

--- examples/basic/src/app.ts
+++ examples/basic/src/app.ts
@@ -2,6 +2,7 @@ import { getTodos, getInputValue, setInputValue, addTodo, toggleTodo, deleteTodo
 import { globalStyles as s, cn } from "./styles.ts";
 import { sortable } from "./sortable.ts";
 
+let visible = false
 
 export const app = div(
   s.body,
@@ -29,37 +30,50 @@ export const app = div(
       ),
       button(s.addButton, "Add", on("click", addTodo))
     ),
-
     div(
-      scope("todos"),
-      s.todoList,
-      list(
-        () => getTodos(),
-        (todo) =>
-          div(
-            sortable(reorderTodos),
-            s.todoItem,
-            span(
-              {
-                "data-drag-handle": "true",
-              },
-              cn(height("20").cursor("grab").width("20").bg("#ccc").display("inline-block")),
-              "⇅"),
-            input(
-              s.checkbox,
-              {
-                type: "checkbox",
-                checked: () => todo.completed,
-              },
-              on("change", () => toggleTodo(todo.id))
-            ),
-            span(
-              () => (todo.completed ? s.todoTextCompleted : s.todoText),
-              () => todo.text
-            ),
-            button(s.deleteButton, "Delete", on("click", () => deleteTodo(todo.id)))
-          )
-      )
-    )
+      input(
+        { type: "checkbox" },
+        on("change", (e) => {
+          visible = (e.target as HTMLInputElement).checked;
+          update();
+        })
+      ),
+      span("is this visible: "),
+      when(() => visible, "yes").else("no"),
+    ),
+    when(() => getTodos().length === 0,
+      when(() => !visible, div("No todos yet! Add one above. 1")).else("2")
+    ).else(
+      div(
+        scope("todos"),
+        () => s.todoList,
+        list(
+          () => getTodos(),
+          (todo) =>
+            div(
+              sortable(reorderTodos),
+              s.todoItem,
+              span(
+                {
+                  "data-drag-handle": "true",
+                },
+                cn(height("20").cursor("grab").width("20").bg("#ccc").display("inline-block")),
+                "⇅"),
+              input(
+                s.checkbox,
+                {
+                  type: "checkbox",
+                  checked: () => todo.completed,
+                },
+                on("change", () => toggleTodo(todo.id))
+              ),
+              span(
+                () => (todo.completed ? s.todoTextCompleted : s.todoText),
+                () => todo.text
+              ),
+              button(s.deleteButton, "Delete", on("click", () => deleteTodo(todo.id)))
+            )
+        )
+      ))
   )
 );
