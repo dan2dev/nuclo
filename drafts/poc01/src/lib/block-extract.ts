68179a3b755edc2768593d6c834f2d26a76bd024
Danilo Castro <dan2dev@outlook.com>
2026-01-07 21:04:40 -0300

    refactor: Improve handling of different BlockMetaData types and update dependencies. (drafts/poc01/src/lib/block-extract.ts, pnpm-lock.yaml)
    - Refactor function `getBlockMetaData` to handle different types of BlockMetaData more efficiently.
    - Update Rollup and its plugins to version 4.55.1, adjust TypeScript packages to version 5.9.3, and enhance ESLint-related dependencies.

--- drafts/poc01/src/lib/block-extract.ts
+++ drafts/poc01/src/lib/block-extract.ts
@@ -1,24 +1,61 @@
-function getBlockMetaData(nodeComment: Comment) {
-  if ((nodeComment as any).metaData) {
-    return (nodeComment as any).metaData as {
-      type: string;
-      id: string;
-      iter: "start" | number;
-    };
+// export type BlockMetaDataType = 1
+export const BlockTypeEnum = {
+  TEXT: 1,
+  LIST: 2,
+  WHEN: 3,
+  PLACEHOLDER: 4,
+} as const;
+export type BlockTypeEnum = typeof BlockTypeEnum[keyof typeof BlockTypeEnum];
+export type BlockMetaDataType = {
+  type: BlockTypeEnum;
+  id?: string;
+};
+
+export type BlockMetaDataStrType = `${number}-${string}-END` | `${number}-${string}-START`;
+export type commentGroup = Node & { nData?: BlockMetaDataType };
+
+
+// comments should be in the format of: {1|2|3|4}-id-END
+function getNData(node: Comment & { nData?: BlockMetaDataType }): BlockMetaDataType | undefined {
+  if (node.nodeType !== Node.COMMENT_NODE) {
+    return undefined;
   }
-  const commentNodeValues = String(nodeComment.nodeValue).split("-");
-  commentNodeValues[2] === "start" ? commentNodeValues[2] : parseInt(commentNodeValues[2], 10);
-  (nodeComment as any).metaData = {
-    type: commentNodeValues[0],
-    id: commentNodeValues[1],
-    iter: commentNodeValues[2] === "start" ? commentNodeValues[2] : parseInt(commentNodeValues[2], 10),
-  };
-  return (nodeComment as any).metaData as {
-    type: string;
-    id: string;
-    iter: "start" | number;
-  };
+  if (node.nData) {
+    return node.nData;
+  }
+  const vArray = String(node.nodeValue).split("-");
+  const type = parseInt(vArray[0], 10) as BlockMetaDataType["type"];
+  const id = vArray[1];
+  const isEnd = Boolean(vArray[vArray.length - 1] === "END");
+  if (isEnd) {
+    return undefined;
+  }
+  node.nData = {
+    type, id,
+  } satisfies BlockMetaDataType;
+  return node.nData;
 }
+
+function openGroupComment(metaData: BlockMetaDataType) {
+  const comment = document.createComment(
+    `${metaData.type}-${metaData.id || ""}-START` satisfies BlockMetaDataStrType,
+  );
+  (comment as any).nData = metaData;
+  return comment;
+}
+
+function closeGroupComment(metaData: BlockMetaDataType) {
+  const comment = document.createComment(
+    `${metaData.type}-${metaData.id || ""}-END` satisfies BlockMetaDataStrType,
+  );
+  (comment as any).nData = {
+    type: metaData.type,
+    id: metaData.id,
+    iter: "end",
+  };
+  return comment;
+}
+
 // block extract will get a parent and a index and will return an array of nodes that are managed by that block
 // this will be used to extract the nodes managed by a block (like a list or a when) for hydration and diffing purposes
 export function extractBlock(parent: HTMLElement, index: number) {
