381cec49742d46349c1b990e115bf2c3920e3dcf
Danilo Castro <dan2dev@outlook.com>
2026-01-07 23:28:56 -0300

    refactor: improve block extraction logic readability
    - Refactor the block extraction logic to enhance readability and maintainability while preserving functionality.

--- drafts/poc01/src/lib/block-extract.ts
+++ drafts/poc01/src/lib/block-extract.ts
@@ -1,83 +1,78 @@
 // export type BlockMetaDataType = 1
 export const BlockTypeEnum = {
-  TEXT: 1,
-  LIST: 2,
-  WHEN: 3,
-  PLACEHOLDER: 4,
+	TEXT: 1,
+	LIST: 2,
+	WHEN: 3,
+	PLACEHOLDER: 4,
 } as const;
-export type BlockTypeEnum = typeof BlockTypeEnum[keyof typeof BlockTypeEnum];
+export type BlockTypeEnum = (typeof BlockTypeEnum)[keyof typeof BlockTypeEnum];
 export type BlockMetaDataType = {
-  type: BlockTypeEnum;
-  id?: string;
+	type: BlockTypeEnum;
+	id?: string;
+	isEnd?: true | undefined;
 };
 
 export type BlockMetaDataStrType = `${number}-${string}-END` | `${number}-${string}-START`;
 export type commentGroup = Node & { nData?: BlockMetaDataType };
 
-
 // comments should be in the format of: {1|2|3|4}-id-END
-function getNData(node: Comment & { nData?: BlockMetaDataType }): BlockMetaDataType | undefined {
-  if (node.nodeType !== Node.COMMENT_NODE) {
-    return undefined;
-  }
-  if (node.nData) {
-    return node.nData;
-  }
-  const vArray = String(node.nodeValue).split("-");
-  const type = parseInt(vArray[0], 10) as BlockMetaDataType["type"];
-  const id = vArray[1];
-  const isEnd = Boolean(vArray[vArray.length - 1] === "END");
-  if (isEnd) {
-    return undefined;
-  }
-  node.nData = {
-    type, id,
-  } satisfies BlockMetaDataType;
-  return node.nData;
+function getNData(node: (Comment | Node) & { nData?: BlockMetaDataType }): BlockMetaDataType | undefined {
+	if (node.nodeType !== Node.COMMENT_NODE) {
+		return undefined;
+	}
+	if (node.nData) {
+		return node.nData;
+	}
+	const vArray = String(node.nodeValue).split("-");
+	const type = parseInt(vArray[0], 10) as BlockMetaDataType["type"];
+	const id = vArray[1];
+	const isEnd = Boolean(vArray[vArray.length - 1] === "END");
+	if (isEnd) {
+		return undefined;
+	}
+	node.nData = {
+		type,
+		id,
+	} satisfies BlockMetaDataType;
+	return node.nData;
 }
 
 function openGroupComment(metaData: BlockMetaDataType) {
-  const comment = document.createComment(
-    `${metaData.type}-${metaData.id || ""}-START` satisfies BlockMetaDataStrType,
-  );
-  (comment as any).nData = metaData;
-  return comment;
+	const comment = document.createComment(`${metaData.type}-${metaData.id || ""}-START` satisfies BlockMetaDataStrType);
+	(comment as any).nData = metaData;
+	return comment;
 }
 
 function closeGroupComment(metaData: BlockMetaDataType) {
-  const comment = document.createComment(
-    `${metaData.type}-${metaData.id || ""}-END` satisfies BlockMetaDataStrType,
-  );
-  (comment as any).nData = {
-    type: metaData.type,
-    id: metaData.id,
-    iter: "end",
-  };
-  return comment;
+	const comment = document.createComment(`${metaData.type}-${metaData.id || ""}-END` satisfies BlockMetaDataStrType);
+	(comment as any).nData = {
+		type: metaData.type,
+		id: metaData.id,
+		iter: "end",
+	};
+	return comment;
 }
 
 // block extract will get a parent and a index and will return an array of nodes that are managed by that block
 // this will be used to extract the nodes managed by a block (like a list or a when) for hydration and diffing purposes
 export function extractBlock(parent: HTMLElement, index: number) {
-  // get the marker node at the specified index
-  const node = parent.childNodes[index];
-  if (!node) {
-    return null;
-  }
-  if (node?.nodeType !== Node.COMMENT_NODE) {
-    const blockMetaData = getBlockMetaData(node as Comment);
+	const nData = getNData(parent.childNodes[index]);
+	if (!nData) {
+		return parent.childNodes[index] || null;
+	}
 
-    const items = new Array<Node>();
-    // if blockIter === "start", needs get all nodes until comment with blockIter "end"
-    if (blockMetaData.iter === "start") {
-      let nextNode = node.nextSibling;
-      while (nextNode) {
-        break;
-      }
-    }
+	// text
+	if (nData.type === BlockTypeEnum.TEXT) {
+		const text = parent.childNodes[index].nextSibling;
+		if (!text || text.nodeType !== Node.TEXT_NODE) {
+			return null;
+		}
+		return parent.childNodes[index].nextSibling;
+	}
 
-    // return Array.from(parent.childNodes).slice(index, index + blockSize);
-  } else {
-    return [node];
-  }
+	// list
+	if (nData.type === BlockTypeEnum.LIST) {
+		const anchor = parent.childNodes[index];
+		// TODO: Implement list extraction
+	}
 }
