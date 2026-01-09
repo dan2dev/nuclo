da0c6a844cb0adac700858de7b085948cf35ceb9
Danilo Castro <dan2dev@outlook.com>
2026-01-09 19:19:07 -0300

    refactor: Update functions for extended metadata response type.
    - Refactor functions to work with an extended metadata response type.

--- drafts/poc01/src/lib/anchor/nuclo-data.ts
+++ drafts/poc01/src/lib/anchor/nuclo-data.ts
@@ -8,13 +8,17 @@ export type BlockTypeEnum = (typeof BlockTypeEnum)[keyof typeof BlockTypeEnum];
 export type BlockMetaDataType = {
   type: BlockTypeEnum;
   id?: string;
-  isEnd?: true | undefined;
+  // isEnd?: true | undefined;
 };
+export type BlockMetaDataResponseType = BlockMetaDataType & { isEnd?: true | undefined };
 
-export type NucloAnchor = Comment & { nData?: BlockMetaDataType, nItems?: Array<WeakRef<Node>> };
+
+export type NucloAnchor = Comment & { nData?: BlockMetaDataResponseType, nItems?: Array<WeakRef<Node>> };
 export type BlockMetaDataStrType = `${number}-${string}-END` | `${number}-${string}-START`;
+
 // comments should be in the format of: {1|2|3|4}-{id}-{START|END}
-export function getAnchorData(node: (Comment | Node) & { nData?: BlockMetaDataType }): BlockMetaDataType | undefined {
+// getAnchorData
+export function getAnchorData(node: (Comment | Node) & { nData?: BlockMetaDataResponseType }): BlockMetaDataResponseType | undefined {
   if (node.nodeType !== Node.COMMENT_NODE) {
     return undefined;
   }
@@ -24,29 +28,28 @@ export function getAnchorData(node: (Comment | Node) & { nData?: BlockMetaDataTy
   const vArray: string[] = String(node.nodeValue).split("-");
   const type: BlockMetaDataType["type"] = parseInt(vArray[0], 10) as BlockMetaDataType["type"];
   const id: string = vArray[1];
-  const isEnd: boolean = Boolean(vArray[vArray.length - 1] === "END");
-  if (isEnd) {
-    return undefined;
-  }
+  const isEnd: true | undefined = Boolean(vArray[vArray.length - 1] === "END") || undefined;
   node.nData = {
     type,
     id,
-  } satisfies BlockMetaDataType;
+    isEnd
+  } satisfies BlockMetaDataResponseType;
   return node.nData;
 }
 
+// createOpenAnchorComment
 export function createOpenAnchorComment(metaData: BlockMetaDataType) {
   const comment = document.createComment(`${metaData.type}-${metaData.id || ""}-START` satisfies BlockMetaDataStrType) as NucloAnchor;
   comment.nData = { ...metaData };
   return comment;
 }
 
+// createClosingAnchorComment
 export function createClosingAnchorComment(metaData: BlockMetaDataType) {
   const comment = document.createComment(`${metaData.type}-${metaData.id || ""}-END` satisfies BlockMetaDataStrType) as NucloAnchor;
   comment.nData = {
-    type: metaData.type,
-    id: metaData.id,
+    ...metaData,
     isEnd: true,
-  };
+  } satisfies BlockMetaDataResponseType;
   return comment;
 }
