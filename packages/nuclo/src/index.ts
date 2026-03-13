906bf5653956f00adb0d5eae16efdba9ffc250a2
Danilo Castro <dan2dev@outlook.com>
2026-03-13 19:59:09 -0300

    chore: Update build process with new tool, simplify scripts, and add TypeScript package settings.
    - Update build and dev scripts to use a new tool called `tsdown` and simplify the build process.
    - Remove Rollup configuration for Nuclo package.
    - Refactor the code to export tag registry, element factory, utility functions, and type guards in a more organized way.
    - Add configuration settings for building two TypeScript packages called 'nuclo' and 'nuclo.ssr'.

--- packages/nuclo/src/index.ts
+++ packages/nuclo/src/index.ts
@@ -1,22 +1,45 @@
 export { initializeRuntime } from "./core/runtimeBootstrap";
-export { registerGlobalTagBuilders, HTML_TAGS, SVG_TAGS, SELF_CLOSING_TAGS } from "./core/tagRegistry";
-export { createHtmlTagBuilder, createSvgTagBuilder } from "./core/elementFactory";
+export {
+  registerGlobalTagBuilders,
+  HTML_TAGS,
+  SVG_TAGS,
+  SELF_CLOSING_TAGS,
+} from "./core/tagRegistry";
+export {
+  createHtmlTagBuilder,
+  createSvgTagBuilder,
+} from "./core/elementFactory";
 export { applyNodeModifier } from "./core/modifierProcessor";
 export { list } from "./list";
 export { when } from "./when";
 export { update } from "./core/updateController";
 export { applyAttributes } from "./core/attributeManager";
-export { appendChildren, createComment, createConditionalComment, replaceNodeSafely } from "./utility/dom";
+export {
+  appendChildren,
+  createComment,
+  createConditionalComment,
+  replaceNodeSafely,
+} from "./utility/dom";
 export { on } from "./utility/on";
 export { scope } from "./utility/scope";
 export { render } from "./utility/render";
-export { isBoolean, isFunction, isNode, isObject, isPrimitive, isTagLike, isZeroArityFunction } from "./utility/typeGuards";
+export {
+  isBoolean,
+  isFunction,
+  isNode,
+  isObject,
+  isPrimitive,
+  isTagLike,
+  isZeroArityFunction,
+} from "./utility/typeGuards";
 export { isBrowser } from "./utility/environment";
-export { createHtmlElementWithModifiers, createSvgElementWithModifiers } from "./internal/applyModifiers";
+export {
+  createHtmlElementWithModifiers,
+  createSvgElementWithModifiers,
+} from "./internal/applyModifiers";
 
 // Style utilities - re-export all from style module
 export * from "./style";
 
 // Auto-initialize when the module is loaded.
 import "./core/runtimeBootstrap";
-
